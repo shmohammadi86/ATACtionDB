3e90db779fcace8a6a02449294c4ec12d147551e
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-02-20 21:12:41 -0500

    Initial commit

---
+++ R/ATACtionDB-package.R
@@ -0,0 +1,21 @@
+#' ATACtionDB: Supplementary data package for the ATACtion package
+#'
+#' Extenstion of ACTIONet package for single-cell epigenomic dataset
+#'
+#' @section Usage: 
+#' 
+#' \enumerate{
+#' \item TBD
+#' }
+#' @section Useful links:
+#'
+#' \enumerate{
+#' \item Report bugs at \url{https://github.com/shmohammadi86/ATACtionDB/issues}
+#' }
+#' 
+#'
+#' @name ATACtionDB
+#' @docType package
+#' @exportPattern "^[[:alpha:]]+"
+NULL
+
---
+++ R/data.R
@@ -0,0 +1,32 @@
+#' Pre-processed collection of enhancers from the Epigenome Roadmap Project (hg19 coordinate)
+#' 
+#' @format:
+#'   SimpleGenomicRangesList with one entry for each sample (tissue/celltype)
+#' 
+#' @source \url{http://www.roadmapepigenomics.org/}
+"EnhDB_Roadmap_hg19"
+
+
+#' Pre-processed collection of enhancers from the Epimap (hg19 coordinate)
+#' 
+#' @format:
+#'   SimpleGenomicRangesList with one entry for each sample (tissue/celltype)
+#' 
+#' @source \url{http://compbio.mit.edu/epimap}
+"EnhDB_Epimap_hg19"
+
+#' Pre-processed collection of enhancers from the Epimap (hg38 coordinate)
+#' 
+#' @format:
+#'   SimpleGenomicRangesList with one entry for each sample (tissue/celltype)
+#' 
+#' @source \url{http://compbio.mit.edu/epimap}
+"EnhDB_Epimap_hg38"
+
+#' Pre-processed LD blocks
+#' 
+#' @format:
+#'   List of independent LD blocks for AFN, ASN, and EUR in GenomicRanges format
+#' 
+#' @source \url{https://bitbucket.org/nygcresearch/ldetect-data/src/master/}
+"LDblocks"
